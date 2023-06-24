359d65bec2fab480bd9a50b3384f0ec27bf68988
Astray <bitcreate@qq.com>
2023-06-24 22:41:57 +0800

    完成个人博客后台管理系统

--- 15. vue从入门到实战/练习/my-website/src/utils/debounce.js
+++
@@ -1,9 +0,0 @@
-export default function debounce(fn, duration = 100) {
-  let timer = null;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      fn(...args);
-    }, duration);
-  };
-}
