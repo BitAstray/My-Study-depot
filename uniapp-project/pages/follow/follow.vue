7f39964c901e11e6d5f41ce0a75e0c56abe536b3
BitCreate <BitCreate@qq.com>
2024-03-19 17:06:24 +0800

    项目结构搭建

---
+++ uniapp-project/pages/follow/follow.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>follow page</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style></style>
---
+++ uniapp-project/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<text>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas accusamus, totam animi qui aut iure labore voluptatibus alias delectus corrupti laboriosam numquam odio nemo
+			provident repudiandae ducimus voluptatum dolore iste.
+		</text>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style></style>
